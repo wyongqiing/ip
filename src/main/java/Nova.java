a0303de8dbfd7538556fa91aac4c2b25171648ef
wyongqiing <wyongqing1016@gmail.com>
2025-01-27 20:26:25 +0800

    Create new chatbot Nova class

---
+++ src/main/java/Nova.java
@@ -0,0 +1,23 @@
+public class Nova {
+    public static void main(String[] args) {
+        // Horizontal lines for aesthetics
+        printHorizontalLine();
+
+        // Greeting message
+        System.out.println(" Hello! I'm Nova");
+        System.out.println(" What can I do for you?");
+        printHorizontalLine();
+
+        // For this skeletal version, the chatbot immediately exits
+        System.out.println(" Bye. Hope to see you again soon!");
+        printHorizontalLine();
+    }
+
+    /**
+     * Prints a horizontal line for visual separation
+     */
+    private static void printHorizontalLine() {
+        System.out.println("____________________________________________________________");
+    }
+
+}
