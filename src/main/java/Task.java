fe1e98b654f0f59c6d64b959910d81cacfaf66f6
wyongqiing <wyongqing1016@gmail.com>
2025-01-27 21:40:52 +0800

    Add feature to task class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,12 +1,25 @@
 public class Task {
     private String description;
+    private boolean isDone;
 
     public Task(String description) {
         this.description = description;
+        this.isDone = false;
     }
 
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[X]" : "[ ]"); // X for done, blank for not done
+    }
     @Override
     public String toString() {
-        return description;
+        return getStatusIcon() + " " + description;
     }
 }
