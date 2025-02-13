f1431e5cd27a02972e26ba5a027df7118da0e930
Yong Qing <wyongqing1016@gmail.com>
2025-02-13 10:37:20 +0800

    Add JavaDoc comments to command classes

--- src/main/java/nova/command/AddTodoCommand.java
+++ src/main/java/nova/command/AddTodoCommand.java
@@ -7,14 +7,31 @@ import nova.task.Todo;
 import nova.ui.Storage;
 import nova.ui.UiNova;
 
+/**
+ * Represents a command to add a Todo task to the task list.
+ */
 public class AddTodoCommand implements Command {
 
     private String description;
 
+    /**
+     * Constructs an AddTodoCommand with the given task description.
+     *
+     * @param description The description of the todo task.
+     */
     public AddTodoCommand(String description) {
         this.description = description;
     }
 
+    /**
+     * Executes the AddTodoCommand by adding a new todo task to the task list.
+     * It also saves the updated task list and notifies the user.
+     *
+     * @param taskList The task list where the new todo task will be added.
+     * @param ui       The UI to display messages to the user.
+     * @param storage  The storage to save the updated task list.
+     * @throws NovaException If the description is empty or an error occurs while saving.
+     */
     @Override
     public void execute(TaskList taskList, UiNova ui, Storage storage) throws NovaException {
         if (description.isEmpty()) {
--- src/main/java/nova/command/Command.java
+++ src/main/java/nova/command/Command.java
@@ -5,9 +5,28 @@ import nova.ui.UiNova;
 import nova.ui.Storage;
 import nova.exception.NovaException;
 
+/**
+ * Represents a command that can be executed within the Nova chatbot.
+ * All command classes must implement this interface.
+ */
 public interface Command {
+
+    /**
+     * Executes the command on the provided task list, UI, and storage.
+     *
+     * @param tasks   The task list on which the command operates.
+     * @param ui      The UI responsible for user interaction.
+     * @param storage The storage to save the updated task list.
+     * @throws NovaException If an error occurs while executing the command.
+     */
     void execute(TaskList tasks, UiNova ui, Storage storage) throws NovaException;
 
+    /**
+     * Determines whether this command signals the termination of the program.
+     * By default, most commands do not exit the program.
+     *
+     * @return {@code true} if the command is an exit command, {@code false} otherwise.
+     */
     default boolean isExit() {
         return false; // Default implementation: most commands are not exit commands
     }
