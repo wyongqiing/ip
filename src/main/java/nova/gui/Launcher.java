7377820a3a836fd5427e1f342af96d907c3ea3f4
Yong Qing <wyongqing1016@gmail.com>
2025-02-24 15:26:08 +0800

    Create a gui package

---
+++ src/main/java/nova/gui/Launcher.java
@@ -0,0 +1,18 @@
+package nova.gui;
+
+import javafx.application.Application;
+
+/**
+ * A nova.gui.Launcher class to launch the chatbot.
+ */
+public class Launcher {
+
+    /**
+     * nova.gui.Main method to run the chatbot.
+     *
+     * @param args Command line arguments.
+     */
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
+}
